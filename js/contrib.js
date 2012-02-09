10898bd67be77a0ac18b4f4ad9edf02e08da66cd
aeoth <paul@theleagueofpaul.com>
2012-02-09 14:28:25 +1100

    Projects now dynamically loaded in via javascript

---
+++ js/contrib.js
@@ -0,0 +1,46 @@
+  function getContribs(projectName) {
+    $.ajax({
+        url: "http://github.com/api/v2/json/repos/show/Code52/" + projectName + "/contributors",
+        dataType: 'jsonp',
+        success: function(data) 
+		{
+			data.contributors = data.contributors.sort(function (a, b) 
+			{ 
+				if (a.contributions > b.contributions) return -1;
+				if (a.contributions < b.contributions) return 1;
+				return 0;
+			});
+			$("#contributors").append("<h2>" + projectName +"</h2>");
+			$("#contributorTemplate").tmpl(data).appendTo("#contributors");
+        }
+    });
+  }
+
+  
+  $(function()
+  {
+   $.ajax({
+        url: "http://github.com/api/v2/json/repos/show/code52",
+        dataType: 'jsonp',
+        success: function(data) 
+		{			  
+			var count = data.repositories.length;
+			data.repositories = data.repositories.sort(function (a, b) 
+			{ 
+				var adate =  new Date(a.created_at).getTime();
+				var bdate =  new Date(b.created_at).getTime();
+				if (adate < bdate) return -1;
+				if (adate > bdate) return 1;
+				return 0;
+			});
+			
+			for(var i = 0; i < count; i++) 
+			{
+				var repo = data.repositories[i];
+				
+				if (repo.name != "code52.github.com")
+					getContribs(repo.name);
+			}
+        }
+  });
+  });
